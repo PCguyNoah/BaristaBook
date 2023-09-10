36e7864a93168ecba2ddc89f0324ed271d421ad7
Jordan Threlfall <jordanjoannthrelfall@gmail.com>
2023-09-09 23:06:03 -0400

    added code to delete contacts

---
+++ LAMPAPI/DeleteContacts.php
@@ -0,0 +1,38 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$id = $inData["id"];
+
+    $conn = new mysqli("localhost", "Group17", "Group17POOSD", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("DELETE from Contacts (ID) VALUES(?)");
+		$stmt->bind_param("s", $id);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+        echo "Successfully Deleted."
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}	
+?>
