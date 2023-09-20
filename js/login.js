19571510dca8cf8dca1bc3e4cb506e9aee963145
Noah Beightol <noahbeightol14@gmail.com>
2023-09-19 22:20:56 -0400

    testing global variable for user model

--- js/login.js
+++ js/login.js
@@ -80,6 +80,66 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// add contact
+document.addEventListener("DOMContentLoaded", function () {
+  const addContactForm = document.getElementById("addContactForm");
+  const resultDiv = document.getElementById("result-contact");
+
+  addContactForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      // Retrieve input values from the form
+      const name = document.getElementById("name").value;
+      const phone = document.getElementById("phone").value;
+      const email = document.getElementById("email").value;
+
+      // set user id
+      console.log("my model:");
+      console.log(window.userModel);
+      const userId = window.userModel.id;
+
+      // Create the contact data object
+      const contactData = {
+          name: name,
+          phone: phone,
+          email: email,
+          userId: userId
+      };
+
+      // Define the API endpoint URL for adding a contact
+      const apiUrl = "http://baristabook.xyz/LAMPAPI/AddContacts.php";
+
+      // Define the request options
+      const requestOptions = {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify(contactData)
+      };
+
+      // Make the API request
+      fetch(apiUrl, requestOptions)
+          .then(response => {
+              if (response.ok) {
+                  return response.json();
+              } else {
+                  throw new Error(`HTTP Error: ${response.status}`);
+              }
+          })
+          .then(data => {
+              console.log(response);
+              resultDiv.innerHTML = "Contact added successfully: " + JSON.stringify(data);
+          })
+          .catch(error => {
+              resultDiv.innerHTML = "Error: " + error.message;
+          });
+
+      // You can clear the form fields if needed
+      addContactForm.reset();
+  });
+});
+
 
 
 
